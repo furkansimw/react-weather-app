fbe28e93f7e1c830d26e1c601b9aee101904a359
simsekfurkann07 <simsekfurkann07@gmail.com>
2022-07-11 02:07:57 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CountriesItem from "./components/CountriesItem";
 
@@ -8,29 +9,29 @@ const App = () => {
   const [country, setCounty] = useState(null);
   const [state, setState] = useState(null);
   const [city, setCity] = useState(null);
-  let _states = [];
-  let _cities = [];
+  const [data, setData] = useState(null);
   useEffect(() => {
     fetch("countries.json")
       .then((res) => res.json())
       .then((res) => setCountries(res));
   }, []);
+  const get = (data) => {
+    console.log(data);
+    const key = "028e5a7aa60025be76785baaa3d08ee0";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.latitude}&lon=${data.longitude}&appid=${key}`;
+    console.log(url);
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  };
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          console.log({ _states });
-        }}
-        style={
-          {
-            position:'fixed',
-            bottom:'10px',
-            right: '10px',
-          }
-        }
-      >
-        Button
-      </button>
+    <div
+      className="App"
+      style={{
+        backgroundColor: data && "#000000",
+        color: data && "#fff",
+      }}
+    >
       <div className="Header">
         <p>Weather App</p>
       </div>
@@ -47,6 +48,32 @@ const App = () => {
           </div>
         </div>
       </div>
+      <div
+        style={{
+          display: data ? "block" : "none",
+        }}
+        className="Preview"
+      >
+   
+          <img
+            src="close.svg"
+            alt="close"
+            style={{
+              width: "36px",
+              cursor: "pointer",
+              height: "36px",
+            }}
+            onClick={() => {
+              setData(null);
+              setCity(null);
+            }}
+          />
+          <div className="data">
+            <h1>{data?.name}</h1>
+            <h2>{(data?.main?.temp - 273.15).toFixed(2)} °</h2>
+          </div>
+        
+      </div>
       <div className="Content">
         <div className="Countries">
           {countries.map((data) => (
@@ -54,8 +81,8 @@ const App = () => {
               data={data}
               key={data.id}
               onClick={() => {
-                setCities([])
-                setCity(null)
+                setCities([]);
+                setCity(null);
                 setCounty(data);
                 fetch("states.json")
                   .then((res) => res.json())
@@ -77,14 +104,12 @@ const App = () => {
               onClick={() => {
                 console.log(data);
                 setState(data);
-                setCity(null)
+                setCity(null);
                 fetch("cities.json")
                   .then((res) => res.json())
                   .then((res) => {
                     setCities(
-                      res.filter(
-                        (_value) => _value.state_name === data.name
-                      )
+                      res.filter((_value) => _value.state_name === data.name)
                     );
                   });
               }}
@@ -98,7 +123,8 @@ const App = () => {
               data={data}
               key={data.id}
               onClick={() => {
-                setCity(data)
+                setCity(data);
+                get(data);
               }}
               active={data === city}
             />
@@ -110,3 +136,4 @@ const App = () => {
 };
 
 export default App;
+//(valNum-32) / 1.8
